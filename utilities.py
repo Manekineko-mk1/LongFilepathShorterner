e15b925f4800795ddac1cdc03bb1c280d4552495
Manekineko-mk1 <HoHoMrChan@gmail.com>
2024-01-11 15:36:40 -0500

    Add function to print file path and filename length

--- utilities.py
+++ utilities.py
@@ -15,3 +15,11 @@ def check_long_path_support(base_dir):
     except OSError as e:
         print("System does not support long paths (> 256 characters).")
         return False
+
+def print_filepath_and_filename_length(file_path):
+    """Print the file path and filename length."""
+    print(f"File path: {file_path} length: {len(file_path)}")
+    print(f"Filename: {os.path.basename(file_path)} length: {len(os.path.basename(file_path))}")
+
+file_path = "C:\\Users\\user\\Documents\\GitHub\\python-snippets\\long_filepath_filename_shortener\\long_sub_dir\\test.txt"
+print_filepath_and_filename_length(file_path)
