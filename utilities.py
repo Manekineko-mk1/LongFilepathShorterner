ef4dae3985985e5fa634be867204fd80457b3018
jesse.tsang@cgi.com <jesse.tsang@cgi.com>
2024-01-16 09:33:56 -0500

    Refactor file path and filename length printing

--- utilities.py
+++ utilities.py
@@ -18,8 +18,18 @@ def check_long_path_support(base_dir):
 
 def print_filepath_and_filename_length(file_path):
     """Print the file path and filename length."""
-    print(f"File path: {file_path} length: {len(file_path)}")
-    print(f"Filename: {os.path.basename(file_path)} length: {len(os.path.basename(file_path))}")
+    file_path_length = len(file_path)
+    filename_length = len(os.path.basename(file_path))
+    file_path_without_filename_length = file_path_length - filename_length
+    
+    if file_path_length >= 230:
+        print(f"File path: {file_path} length: {file_path_length}")
+    
+    if filename_length >= 50:
+        print(f"Long Filename Detected: {os.path.basename(file_path)} length: {filename_length}")
+    
+    if file_path_length >= 200 and filename_length >= file_path_without_filename_length:
+        print(f"Filename >= Directories Path: {os.path.basename(file_path)} length: {filename_length} | Directories Path length: {file_path_without_filename_length}")
 
 def read_filepaths_from_txt(txt_file):
     """Read file paths from a text file."""
@@ -33,4 +43,4 @@ def read_filepaths_from_txt(txt_file):
             print_filepath_and_filename_length(file_path)
 
 txt_file = "test_filepaths.txt"
-read_filepaths_from_txt(txt_file)
+# read_filepaths_from_txt(txt_file)
